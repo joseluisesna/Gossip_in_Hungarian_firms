ef9724384fd39bc6574ccf97c78d9545cff24f79
José Luis Estévez <50023683+joseluisesna@users.noreply.github.com>
2021-02-25 10:55:20 +0100

    Update 1_Data_tidying.R

--- 1_Data_tidying.R
+++ 1_Data_tidying.R
@@ -9,13 +9,12 @@
 library(ggplot2)
 
 # DATA LOADING AND DATA TIDYING
+rm(list=ls())
 load('data.RData')
 rm(list=ls()[-c(1,13,20)]) # keep only attributes, networks, and gossip
-attributes <- attributes[,1:75] # # exclusion of network variables from attributes
+attributes <- attributes[,1:75] # # exclusion of relational variables from the object containing the attributes
 
-########################################################################################################################
-
-# Leaders per setting (fix)
+# Leaders per setting (fixing this variable which, in the original data set, is inaccurate)
 for(i in 1:nrow(attributes)){
   if(!is.na(stringr::str_detect(attributes$hr_job[[i]],' '))){
     if(stringr::str_detect(attributes$hr_job[[i]],c('Manager|Chief|Director|director|leader'))){
@@ -26,10 +25,14 @@ for(i in 1:nrow(attributes)){
 
 ########################################################################################################################
 
-# Some general information
-organisation_ID <- unique(na.omit(attributes$group)) # 9 units or groups
-employee_ID <- attributes$responder # N=225, but observe there are 2 duplicated employees: 1F6032 and 1F6033
-networks_available <- unique(networks$network_questionID_EN) # 43 networks available in the data set
+# GENERAL INFORMATION
+
+(organisation_ID <- unique(na.omit(attributes$group))) # 9 units or firms/departments
+(employee_ID <- attributes$responder) # N=225, but 2 duplicated employees: 1F6032 and 1F6033
+(networks_available <- unique(networks$network_questionID_EN)) # 43 relational variables per unit available 
+# "received_personal_info" refers to the gossip, so it was excluded from the set of relational variables (42 variables)
+networks <- networks[networks$network_questionID_EN != "received_personal_info",]
+networks_available <- networks_available[-42]
 
 # Fixing duplicated employees in all three objects: attributes, networks, and gossip
 '%!in%' <- function(x,y)!('%in%'(x,y))
@@ -150,22 +153,19 @@ for(i in 1:nrow(networks)){
   networks_mtx[[gro]][[mtx]][row,col] <- networks$tie[i]
 }
 
-rm(duplicated_actors);rm(missing_data);rm(valid_triplets);rm(valid_ties);rm(employee_ID);rm(networks)
-rm(i);rm(j);rm(gro);rm(mtx);rm(row);rm(col)
-
 ########################################################################################################################
 
 # CHECKING THE GOSSIP CUBE
 
-gos <- gos_pos <- gos_neg <- gos_neu <- gossip_cube
+gos <- gos_pos <- gos_neg <- gossip_cube
 
 for(x in seq_along(gossip_cube)){
   for(i in 1:nrow(gossip_cube[[x]])){
     for(j in 1:nrow(gossip_cube[[x]])){
       for(k in 1:nrow(gossip_cube[[x]])){
         if(i != j & i != k & j != k){
-          # the gossip cube (no matter the tone)
-          if(!is.na(gossip_cube[[x]][i,j,k])){
+          # the gossip cube (either positive or negative gossip, but not neutral)
+          if(!is.na(gossip_cube[[x]][i,j,k]) & (gossip_cube[[x]][i,j,k] == 1 | gossip_cube[[x]][i,j,k] == -1)){
             gos[[x]][i,j,k] <- 1
           }else{
             gos[[x]][i,j,k] <- 0
@@ -182,14 +182,18 @@ for(x in seq_along(gossip_cube)){
           }else{
             gos_neg[[x]][i,j,k] <- 0
           }
-          # the neutral gossip cube
-          if(!is.na(gossip_cube[[x]][i,j,k]) & gossip_cube[[x]][i,j,k] == 0){
-            gos_neu[[x]][i,j,k] <- 1
-          }else{
-            gos_neu[[x]][i,j,k] <- 0
-          }
         }
       }
+    }
+  }
+}
+
+# Allocation of missing data (if a respondent was missing, all sender-target ties involving this respondent are missing)
+
+for(x in seq_along(gossip_cube)){
+  for(i in rownames(gossip_cube[[x]])){
+    if(i %in% missing_respondents){
+      gos[[x]][,i,] <- gos_pos[[x]][,i,] <- gos_neg[[x]][,i,] <-  gos[[x]][,i,]*NA
     }
   }
 }
@@ -198,43 +202,35 @@ for(x in seq_along(gossip_cube)){
 rec_sen <- gos
 rec_sen_pos <- gos_pos
 rec_sen_neg <- gos_neg
-rec_sen_neu <- gos_neu
 
 for(x in seq_along(rec_sen)){
   rec_sen[[x]] <- apply(rec_sen[[x]],c(2,1),max,na.rm=TRUE) # receiver-sender matrix
   rec_sen_pos[[x]] <- apply(rec_sen_pos[[x]],c(2,1),max,na.rm=TRUE)
   rec_sen_neg[[x]] <- apply(rec_sen_neg[[x]],c(2,1),max,na.rm=TRUE)
-  rec_sen_neu[[x]] <- apply(rec_sen_neu[[x]],c(2,1),max,na.rm=TRUE)
   diag(rec_sen[[x]]) <- 0
   diag(rec_sen_pos[[x]]) <- 0
   diag(rec_sen_neg[[x]]) <- 0
-  diag(rec_sen_neu[[x]]) <- 0
   rec_sen[[x]] <- rowSums(rec_sen[[x]])
   rec_sen_pos[[x]] <- rowSums(rec_sen_pos[[x]])
   rec_sen_neg[[x]] <- rowSums(rec_sen_neg[[x]])
-  rec_sen_neu[[x]] <- rowSums(rec_sen_neu[[x]])
 }
 
-gossip_sum <- as.data.frame(matrix(NA,nrow=length(rec_sen),ncol=10,
-                                   dimnames=list(names(rec_sen),c('triads','triads_pos','triads_neg','triads_neu',
-                                                                  'triads_total','reporters','reporters_pos',
-                                                                  'reporters_neg','reporters_neu','reporters_total'))))
+gossip_sum <- as.data.frame(matrix(NA,nrow=length(rec_sen),ncol=8,
+                                   dimnames=list(names(rec_sen),c('triads','triads_pos','triads_neg','triads_total',
+                                                                  'reporters','reporters_pos','reporters_neg','nodes'))))
+
 for(x in seq_along(rec_sen)){
-  # summary of triads: with gossip, positive or negative, and total possible
-  gossip_sum[x,'triads'] <- sum(gos[[x]],na.rm=TRUE)
-  gossip_sum[x,'triads_pos'] <- sum(gos_pos[[x]],na.rm=TRUE)
-  gossip_sum[x,'triads_neg'] <- sum(gos_neg[[x]],na.rm=TRUE)
-  gossip_sum[x,'triads_neu'] <- sum(gos_neu[[x]],na.rm=TRUE)
-  gossip_sum[x,'triads_total'] <- nrow(gos[[x]]) * (nrow(gos[[x]])-1) * (nrow(gos[[x]])-2)
-  # number of respondents reporting those triads
-  gossip_sum[x,'reporters'] <- sum(rec_sen[[x]] != 0)
-  gossip_sum[x,'reporters_pos'] <- sum(rec_sen_pos[[x]] != 0)
-  gossip_sum[x,'reporters_neg'] <- sum(rec_sen_neg[[x]] != 0)
-  gossip_sum[x,'reporters_neu'] <- sum(rec_sen_neu[[x]] != 0)
-  gossip_sum[x,'reporters_total'] <- length(rec_sen[[x]])
+  gossip_sum[x,'triads'] <- sum(gos[[x]],na.rm=TRUE) # gossip triads
+  gossip_sum[x,'triads_pos'] <- sum(gos_pos[[x]],na.rm=TRUE) # positive gossip triads
+  gossip_sum[x,'triads_neg'] <- sum(gos_neg[[x]],na.rm=TRUE) # negative gossip triads
+  gossip_sum[x,'triads_total'] <- sum(!is.na(gos[[x]])) # potential gossip triads
+  gossip_sum[x,'reporters'] <- sum(rec_sen[[x]] != 0) # number of gossip reporters
+  gossip_sum[x,'reporters_pos'] <- sum(rec_sen_pos[[x]] != 0) # number of pos. gossip reporters
+  gossip_sum[x,'reporters_neg'] <- sum(rec_sen_neg[[x]] != 0) # number of neg. gossip reporters 
+  gossip_sum[x,'nodes'] <- length(rec_sen[[x]]) # nodes in the network
 }
 
-View(gossip_sum)
+gossip_sum
 
 ########################################################################################################################
 
@@ -242,9 +238,9 @@ View(gossip_sum)
 
 desc_receiver <- vector('list',length=length(gossip_cube))
 for(i in seq_along(gossip_cube)){
-  desc_receiver[[i]] <- as.data.frame(matrix(NA,nrow=nrow(gossip_cube[[i]]),ncol=5))
+  desc_receiver[[i]] <- as.data.frame(matrix(NA,nrow=nrow(gossip_cube[[i]]),ncol=4))
   rownames(desc_receiver[[i]]) <- rownames(gossip_cube[[i]])
-  colnames(desc_receiver[[i]]) <- c('network','dyads','dyads_pos','dyads_neg','dyads_neu')
+  colnames(desc_receiver[[i]]) <- c('network','dyads','dyads_pos','dyads_neg')
   desc_receiver[[i]]$network <- names(gossip_cube)[i]
 }
 
@@ -254,7 +250,6 @@ for(x in seq_along(desc_receiver)){
     desc_receiver[[x]]$dyads[i] <- sum(gos[[x]][,i,],na.rm=TRUE)
     desc_receiver[[x]]$dyads_pos[i] <- sum(gos_pos[[x]][,i,],na.rm=TRUE)
     desc_receiver[[x]]$dyads_neg[i] <- sum(gos_neg[[x]][,i,],na.rm=TRUE)
-    desc_receiver[[x]]$dyads_neu[i] <- sum(gos_neu[[x]][,i,],na.rm=TRUE)
   }
 }
 
@@ -271,8 +266,6 @@ grid.background <- theme_bw()+
 
 jpeg(filename='Receiver-specific gossip.jpeg',width=11,height=6,units='in',res=1000)
 ggplot(data=desc_receiver)+
-  geom_point(aes(x=subject,y=dyads_neu),size=2)+
-  geom_point(aes(x=subject,y=dyads_neu),size=1,colour='royalblue')+
   geom_point(aes(x=subject,y=dyads_neg),size=2)+
   geom_point(aes(x=subject,y=dyads_neg),size=1,colour='red')+
   geom_point(aes(x=subject,y=dyads_pos),size=2)+
@@ -284,7 +277,7 @@ dev.off()
 
 ########################################################################################################################
 
-# Need to exclude networks F106b-c-d (gossip reported by less than 1/2 of the sample in these three units)
+# Need to exclude networks F106b-c-d (gossip reported by very few individuals)
 excl <- c('F106b','F106c','F106d')
 attributes <- attributes[attributes$group %!in% excl,]
 missing_respondents <- missing_respondents[missing_respondents %in% attributes$responder]
@@ -300,32 +293,10 @@ organisation_ID <- organisation_ID[organisation_ID %!in% excl]
 
 ########################################################################################################################
 
-# INCONSISTENCIES IN GOSSIP?
-
-# Senders conveying both positive and negative sentiments about the same target?
-gos_pos <- as.data.frame(gossip[gossip$info == 1,])
-gos_neg <- as.data.frame(gossip[gossip$info == -1,])
-
-for(i in 1:nrow(gos_pos)){
-  gos_pos$st_dyad[i] <- paste(gos_pos$sender[i],gos_pos$target[i],sep=';') 
-}
-for(i in 1:nrow(gos_neg)){
-  gos_neg$st_dyad[i] <- paste(gos_neg$sender[i],gos_neg$target[i],sep=';') 
-}
-
-intersect_posneg <- intersect(gos_pos$st_dyad,gos_neg$st_dyad) # 48 inconsistencies
-inconst_gos <- rbind(gos_pos[gos_pos$st_dyad %in% intersect_posneg,],gos_neg[gos_neg$st_dyad %in% intersect_posneg,])
-inconst_gos$neg <-  1*inconst_gos$info == -1 # negative
-
-# Inconsistencies for a total of 130 cases regarding...
-length(unique(inconst_gos$sender)) # 29 unique senders
-length(unique(inconst_gos$target)) # 36 unique targets
-
-########################################################################################################################
-
 # Removal of unnecessary objects
-rm(rec_sen);rm(rec_sen_pos);rm(rec_sen_neg);rm(rec_sen_neu);rm(desc_receiver);rm(gos);rm(excl);rm(i);rm(j);rm(k);rm(x)
-rm(grid.background);rm(gos_pos);rm(gos_neg);rm(gos_neu);rm(intersect_posneg);rm(inconst_gos)
+rm(duplicated_actors);rm(missing_data);rm(valid_triplets);rm(valid_ties);rm(employee_ID);rm(networks);rm(gro);rm(mtx)
+rm(row);rm(col);rm(rec_sen);rm(rec_sen_pos);rm(rec_sen_neg);rm(desc_receiver);rm(gos);rm(excl);rm(i);rm(j);rm(k);rm(x)
+rm(gossip);rm(gossip_cube);rm(grid.background)
 
 # Save image
 save.image('tidieddata.RData')
